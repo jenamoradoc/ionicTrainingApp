6ef4ccfcb4591e03c855d39d1d9e5d304f3d6767
jenamorado <jenamorado@grupoassa.com>
2018-08-08 11:00:11 -0300

    Tabs & Navegation

---
+++ src/pages/pagina2/pagina2.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+import { Pagina3Page } from '../index.paginas';
+
+
+
+@IonicPage()
+@Component({
+  selector: 'page-pagina2',
+  templateUrl: 'pagina2.html',
+})
+export class Pagina2Page {
+
+  pagina3:any = Pagina3Page;
+
+  mutantes:any[]= [
+
+    {
+      nombre:"Magneto",
+      poder:"Controlar metales"
+    },
+    {
+      nombre:"Wolverine",
+      poder:"Regeneracion Acelerada"
+    },
+    {
+      nombre:"Profesor X",
+      poder:"Poderes Psiquicos"
+    },
+    {
+      nombre:"Mystique",
+      poder:"Cambiar de Apareciencia"
+    }
+
+  ]
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad Pagina2Page');
+  }
+  
+  irPagina3( mutante:any ){
+    console.log( mutante);
+   
+    this.navCtrl.push( Pagina3Page, { 'mutante': mutante} );
+
+  }
+
+}
